0efa594b2d38bf4905f440d8b466608567116e27
Rapid-C0de <rafidasyrafa07@gmail.com>
2024-09-09 10:39:30 +0700

    create gudang migration

---
+++ database/migrations/2024_09_09_033402_gudang.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gudang', function (Blueprint $table) {
+            $table->string('id', 36)->primary();
+            $table->string('nama_gudang');
+            $table->string('alamat');
+            $table->string('no_telp');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('created_by', 36)->nullable();
+            $table->string('edited_by', 36)->nullable();
+            $table->string('deleted_by', 36)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gudang');
+    }
+};
