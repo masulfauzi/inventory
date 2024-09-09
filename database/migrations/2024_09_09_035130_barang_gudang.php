8ae6f55810660f4e29a4ad5a6d0c31610057af46
Balqis Putri <156891963+balqisputri@users.noreply.github.com>
2024-09-09 10:58:51 +0700

    create barang_gudang

---
+++ database/migrations/2024_09_09_035130_barang_gudang.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang_gudang', function (Blueprint $table) {
+            $table->string('id', 36)->primary();
+            $table->string('id_barang', 36);
+            $table->foreign('id_barang')->references('id')->on('barang')->onUpdate('cascade')->onDelete('restrict');
+            $table->string('id_gudang', 36);
+            $table->foreign('id_gudang')->references('id')->on('gudang')->onUpdate('cascade')->onDelete('restrict');
+            $table->integer('stok');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('created_by', 36)->nullable();
+            $table->string('edited_by', 36)->nullable();
+            $table->string('deleted_by', 36)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
