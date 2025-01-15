0a9cb5fd2c2de4d0a4553a8bdad103c44e569d97
auliafia <auliafiasugiyanto@gmail.com>
2025-01-15 17:06:19 +0700

    update app.blade
    ubah dark mode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <title>{{ config('app.name') }} | {{ @$title ?? 'Dashboard' }}</title>
 
     <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
-    {{-- <link rel="stylesheet" href="assets/css/main/app-dark.css"> --}}
+    <link rel="stylesheet" href="assets/css/main/app-dark.css">
     <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.svg') }}" type="image/x-icon">
     <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.png') }}" type="image/png">
     <link rel="stylesheet" href="{{ asset('assets/css/pages/fontawesome.css') }}">
