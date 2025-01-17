421765231d8d802dde14f00cea4db03052dd167e
auliafia <auliafiasugiyanto@gmail.com>
2025-01-17 14:42:52 +0700

    edit dashboard
    edit dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <title> {{  config('app.name', 'Laralag') }}</title>
     
     <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/app.css') }}">
     <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.svg') }}" type="image/x-icon">
     <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.png') }}" type="image/png">
     <link rel="stylesheet" href="{{ asset('assets/css/shared/iconly.css') }}">
