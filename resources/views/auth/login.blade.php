81409073842e875e6f596bd15a60002d803f6a7e
Reyfalino <155606487+Reyfalino@users.noreply.github.com>
2025-01-20 16:39:40 +0700

    Edit
    Menghilangkan Mazer

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,10 +3,9 @@
     <div class="row h-100">
         <div class="col-lg-5 col-12">
             <div id="auth-left">
-                <div class="auth-logo">
-                    <a href="/"><img src="{{ asset('assets/images/logo/logo.svg') }}" alt="Logo"></a>
-                </div>
+                
                 <h1 class="auth-title">Log in</h1>
+                <p class="auth-subtitle mb-5"></p>
                 {{-- <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p> --}}
 
                 <!-- Session Status -->
